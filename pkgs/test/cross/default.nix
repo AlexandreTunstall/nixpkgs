6f791b432bd07e33f27a738f0d2f171f3a28de15
Alex Tunstall <alex@tunstall.xyz>
2024-03-29 10:05:19 +0000

    tests.cross.sanity: Add GHC to catch regressions

--- pkgs/test/cross/default.nix
+++ pkgs/test/cross/default.nix
@@ -146,6 +146,7 @@ let
     pkgs.pkgsCross.mips64el-linux-gnuabi64.stdenv
     pkgs.pkgsCross.mips64el-linux-gnuabin32.stdenv
     pkgs.pkgsCross.mingwW64.stdenv
+    pkgs.pkgsCross.riscv64.haskell.compiler.native-bignum.ghc948
 
   ] ++ lib.optionals (with pkgs.stdenv.buildPlatform; isx86_64 && isLinux) [
     # Musl-to-glibc cross on the same architecture tends to turn up
